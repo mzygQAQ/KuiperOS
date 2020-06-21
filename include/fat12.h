a0c2f39a9495959ccda084d7041f406946fddc59
guoxiang2 <guoxiang2@yy.com>
2020-06-21 12:17:12 +0800

    fatRootEntry

--- include/fat12.h
+++ include/fat12.h
@@ -1,8 +1,20 @@
-#ifndef __FAT12_H__
-#define __FAT12_H__
+#ifndef _FAT12_H
+#define _FAT12_H
+
+#include "types.h"
 
 typedef struct {
 
 } fat12_header_t;
 
-#endif /* end of __FAT12_H__ */
+typedef struct {
+	int8_t 	 DIR_Name[11];	// file name
+	uint8_t  DIR_Attr;		// file attr
+	uint8_t  reserve[10];	// not use
+	uint16_t DIR_WrtTime;	// last write time
+	uint16_t DIR_WrtDate;	// last write date
+	uint16_t DIR_FstClus;	// file start position
+	uint32_t DIR_FileSize;	// file size
+} fat12_root_entry_t;
+
+#endif /* end of _FAT12_H */
