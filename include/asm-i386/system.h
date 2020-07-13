b64bea49de900038db159f26945f662759a13f45
guoxiang2 <guoxiang2@yy.com>
2020-07-13 15:42:22 +0800

    mb-i386

---
+++ include/asm-i386/system.h
@@ -0,0 +1,34 @@
+#ifndef __ASM_I386_SYSTEM_H__
+#define __ASM_I386_SYSTEM_H__
+
+/*
+ * The code implementation here refers to the Linux kernel to a 
+ * certaion extent, and the copyright belongs to the original author.
+ */
+
+#ifdef __GNUC__ 
+#define barrier()	__asm__ __volatile__("" ::: "memory")
+#else
+#error gcc compiler required.
+#endif
+
+#define nop() __asm__ __volatile__ ("nop")
+
+#define mb() 		barrier()
+#define rmb()		barrier()
+#define wmb()		barrier()
+
+#ifdef CONFIG_SMP
+#define smp_mb() 	mb()
+#define smp_rmb() 	rmb()
+#define smp_wmb()	wmb()
+#define mb_set_val(var, value)  do{ var=value; barrier(); } while(0)
+#else
+#define smp_mb()	barrier()
+#define smp_rmb()	barrier()
+#define smp_wmb()	barrier()
+#define mb_set_val(var, value)  do{ var=value;barrier(); } while(0)
+#endif
+
+
+#endif
